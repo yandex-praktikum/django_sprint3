6d2cd7faed431d74d33ffd1bbcce2efa5bb277fe
RomanGolubkov <158145501+RomanGolubkov@users.noreply.github.com>
2024-05-13 10:25:47 +0300

    Update views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -6,18 +6,19 @@ from django.utils.timezone import now
 from .models import Post, Category
 
 
-def published_post():
-    return Post.objects.select_related(
+def get_published_posts(category_posts):
+    category_posts = Post.objects.select_related(
         'author', 'category', 'location'
     ).filter(
         is_published=True,
         pub_date__lte=now(),
         category__is_published=True,
     )
+    return category_posts
 
 
 def index(request):
-    post_list = published_post()[:settings.POSTS_BY_PAGE]
+    post_list = published_post(category_posts)[:settings.POSTS_BY_PAGE]
     return render(
         request, 'blog/index.html', context={'post_list': post_list}
     )
@@ -25,7 +26,7 @@ def index(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(
-        published_post(),
+        published_post(category_posts),
         pk=post_id,
     )
     return render(request, 'blog/detail.html', context={'post': post})
@@ -38,10 +39,8 @@ def category_posts(request, category_slug):
             is_published=True
         )
     )
-    post_list = category.post.all().select_related(
-        'author', 'location').filter(
-            pub_date__lte=now(),
-            is_published=True,
+    post_list = get_published_posts(category_posts).filter(
+        category__slug=category_slug
     )
     return render(request, 'blog/category.html',
                   context={'category': category, 'post_list': post_list})
